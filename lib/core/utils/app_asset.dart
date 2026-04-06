9396c76df7bf920e45051f3c2189b3e4e087d75b
Legion <mohamed.abdou50050@gmail.com>
2026-04-06 13:48:19 +0200

    make custom dialogutils and toast and textformfield and make classes validator and auth_layout and make responsive ui of login ,register

---
+++ lib/core/utils/app_asset.dart
@@ -0,0 +1,18 @@
+class AppAsset {
+  static const String splashImage = "assets/images/matgary_splash.png";
+  static const String ads1 = "assets/images/ads_1.png";
+  static const String ads2 = "assets/images/ads_2.png";
+  static const String ads3 = "assets/images/ads_3.png";
+  static const String category = "assets/images/category.png";
+  static const String category_selected = "assets/images/category_selected.png";
+  static const String favorite = "assets/images/favorite.png";
+  static const String favorite_selected = "assets/images/favorite_selected.png";
+  static const String home = "assets/images/home.png";
+  static const String home_selected = "assets/images/home_selected.png";
+  static const String profile = "assets/images/profile.png";
+  static const String profile_unselected =
+      "assets/images/profile_unselected.png";
+  static const String product1 = "assets/images/product_1.jpg";
+  static const String product2 = "assets/images/product_2.jpg";
+  static const String product3 = "assets/images/product_3.jpg";
+}
